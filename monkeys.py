13c0898892ff34881fe4c18830b2eb29aab9a90d
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-02-05 23:24:35 -0500

    Method stubs in the monkeys.py module.

---
+++ monkeys.py
@@ -0,0 +1,39 @@
+
+def char2index(c):
+    pass
+
+
+def index2char(i):
+    pass
+
+
+def compute_freq_tab(order, *corpus_file):
+    pass
+
+
+def write_freq_tab(order, freq_tab, output_file):
+    pass
+
+
+def read_freq_tab(input_file):
+    pass
+
+
+def most_probable_digraph(freq_tab, initial_char):
+    pass
+
+
+def simulate(order, freq_tab, resolution, num_chars, output_file):
+    pass
+
+
+def relative_word_yield(sim_file, corpus_file):
+    pass
+
+
+def create_profile(order, freq_tab, profile_len):
+    pass
+
+
+def profile_dissimilarity(profile1, profile2):
+    pass
